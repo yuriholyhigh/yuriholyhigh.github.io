81e9c548fbaf4a26f1716b9898d0c66405e622c5
张少良 <yurizhang.110@gmail.com>
2017-03-18 03:46:56 +0800

    test page

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhangshaoliang on 2017/3/18.
+ */
+
+window.onload = function () {
+    document.getElementById("view-title").onclick = function () {
+        document.getElementById("view-title").style.color = "red";
+        alert("Welcome!");
+    }
+};
